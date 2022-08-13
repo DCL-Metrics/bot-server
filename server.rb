1cd7334855b7fe78f549ab803b6f5690a4a7b9ad
dax <d.dax@email.com>
2022-08-13 23:19:23 +0200

    rewind body

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require_relative 'lib/telegram/internal'
 
 class Server < Sinatra::Application
   post '/telegram/internal' do
-    request.rewind
+    request.body.rewind
     data = JSON.parse(request.body.read)
 
     Telegram::Internal.handle_request(data).send
